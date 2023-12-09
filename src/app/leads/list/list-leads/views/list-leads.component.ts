f466f40db5084c967f1ba9d1042020f52255fd72
cristianomartinsdias82 <crisdi@gmail.com>
2023-12-09 20:22:36 -0300

    WIP

--- src/app/leads/list/list-leads/views/list-leads.component.ts
+++ src/app/leads/list/list-leads/views/list-leads.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTable } from "@angular/material/table";
@@ -7,24 +7,18 @@ import { LeadsService } from "src/app/leads/shared/services/leads.service";
 import { NotificationStickerService } from "src/app/shared/ui/widgets/notification-sticker/notification-sticker.service";
 import { Lead } from "src/app/leads/shared/models/lead";
 import { PromptService } from "src/app/shared/ui/widgets/prompt-dialog/prompt.service";
-import { OneTimePasswordService } from "src/app/core/security/authorization/components/one-time-password/one-time-password.service";
-import { environment } from "src/environments/environment";
-import { Permissions } from "src/app/core/security/permissions";
-import { NotificationPanelService } from "src/app/shared/ui/widgets/notification-panel/notification-panel.service";
-import { ErrorMessages } from "src/app/leads/shared/messages/error-messages";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "ldm-list-leads",
   templateUrl: "./list-leads.component.html",
   styleUrls: ["./list-leads.component.scss"],
 })
-export class ListLeadsComponent implements AfterViewInit {
+export class ListLeadsComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(
     private leadsService: LeadsService,
-    private notificationStickerService: NotificationStickerService,
-    private notificationPanelService: NotificationPanelService,
     private promptService: PromptService,
-    private oneTimePasswordService: OneTimePasswordService
+    private notificationStickerService: NotificationStickerService
   ) {}
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -33,6 +27,22 @@ export class ListLeadsComponent implements AfterViewInit {
 
   displayedColumns = ["cnpj", "razaoSocial", "cep", "estado", "acoes"];
   listLeadsDataSource: ListLeadsDataSource = null!;
+  onLeadRemoveSuccessfulSubsc!: Subscription;
+
+  ngOnInit() {
+    
+    this.onLeadRemoveSuccessfulSubsc = this.leadsService
+          .onLeadRemoveSuccessful$
+          .subscribe({ next: () => {
+              this.notificationStickerService.show("Lead removido com sucesso.");  
+              setTimeout(() => this.reloadView(), 750);
+            }
+          });
+  }
+
+  ngOnDestroy(): void {
+    this.onLeadRemoveSuccessfulSubsc.unsubscribe();
+  }
 
   ngAfterViewInit() {
     this.listLeadsDataSource = new ListLeadsDataSource(
@@ -49,56 +59,7 @@ export class ListLeadsComponent implements AfterViewInit {
   }
 
   onDeleteItemClick(lead: Lead) {
-    this.promptService.openYesNoDialog(
-      `Deseja realmente remover o lead '${lead.razaoSocial}'?`,
-      () => { this.removeLead(lead); },
-      () => {},
-      "Confirmação de exclusão"
-    );
-  }
-
-  //TODO: Your next assignment is to move all this messy code away from the component code. Abstract it away from here by placing it inside
-  //the LeadsService class
-  removeLead(lead: Lead) {
-    this.leadsService
-            .removeLead(lead.id!, lead.revision!)
-            .subscribe({
-              next: () => {
-                this.notificationStickerService.show("Lead removido com sucesso.");
-
-                this.reloadView();
-              },
-              error: (err) => {
-                
-                let message = '';
-                let displayOneTimePasswordDialog = true;
-
-                switch (err.statusCode) {
-                  case environment.oneTimePassword.otpInvalidStatusCode: { message = ErrorMessages.CodigoInvalido; break; }
-                  case environment.oneTimePassword.otpExpiredStatusCode: { message = ErrorMessages.CodigoInvalido; break; }
-                  case environment.oneTimePassword.otpChallengeStatusCode: { message = ''; break; }
-                  default: {
-                    displayOneTimePasswordDialog = false;
-
-                    //TODO: Implement a Global error handler
-                    //TODO: Next, throw an error from here so the global error handler can catch and handle it more properly
-                    this.notificationPanelService.show(`${ErrorMessages.ErroAoProcessarSolicitacao}. ${ErrorMessages.EntreEmContatoSuporteAdm}`, null!, null!);
-                  }
-                }
-
-                if (displayOneTimePasswordDialog) {
-                  
-                  setTimeout(() => {
-                    this.oneTimePasswordService
-                          .openDialog({
-                              onSendCodeRequest: () => { this.removeLead(lead); },
-                              resource: Permissions.Delete
-                          })
-                          .subscribe(_ => this.oneTimePasswordService.setMessage(message));
-                  }, 100);
-                }
-              }
-            });
+    this.leadsService.removeLead(lead);
   }
 
   reloadView() {
--- src/app/leads/shared/services/leads.service.ts
+++ src/app/leads/shared/services/leads.service.ts
@@ -8,6 +8,8 @@ import { ApplicationResponse } from "src/app/shared/core/api-response/applicatio
 import { RevisionUpdate } from "./conflict-resolution/revision-update";
 import { Permissions } from "src/app/core/security/permissions";
 import { OneTimePasswordService } from "src/app/core/security/authorization/components/one-time-password/one-time-password.service";
+import { ErrorMessages } from "../messages/error-messages";
+import { PromptService } from "src/app/shared/ui/widgets/prompt-dialog/prompt.service";
 
 @Injectable({
   providedIn: "root",
@@ -17,12 +19,19 @@ export class LeadsService extends DataService<Lead> {
   private static LeadEndpoint = "leads";
   constructor(
     httpClient: HttpClient,
-    private oneTimePasswordService: OneTimePasswordService) {
+    private oneTimePasswordService: OneTimePasswordService,
+    private promptService: PromptService)
+  {
+
     super(httpClient, LeadsService.LeadEndpoint);
+
   }  
 
   private leadRevisionUpdateSubscription = new Subject<RevisionUpdate>();
   onLeadRevisionUpdate$ = this.leadRevisionUpdateSubscription.asObservable();
+
+  private leadRemoveSuccessSubscription = new Subject<{}>();
+  onLeadRemoveSuccessful$ = this.leadRemoveSuccessSubscription.asObservable();
 
   setLeadNewRevision(revisionUpdate: RevisionUpdate) {
     this.leadRevisionUpdateSubscription.next(revisionUpdate);
@@ -52,17 +61,65 @@ export class LeadsService extends DataService<Lead> {
     );
   }
 
-  removeLead(leadId: string, revision: string): Observable<ApplicationResponse<any>> {
+  removeLead(lead: Lead, skipConfirmation = false) {
 
-    return this.httpClient
-               .delete<ApplicationResponse<Lead>>(`${environment.apiUrl}/${LeadsService.LeadEndpoint}/${leadId}?revision=${encodeURIComponent(revision)}`,
-               {
-                headers: {
-                  resource : Permissions.Delete,
-                  otp: this.oneTimePasswordService.getInformedCode()
+    const action = () => {
+      this.httpClient
+          .delete<ApplicationResponse<Lead>>(`${environment.apiUrl}/${LeadsService.LeadEndpoint}/${lead.id!}?revision=${encodeURIComponent(lead.revision!)}`,
+          {
+            headers: {
+              resource : Permissions.Delete,
+                otp: this.oneTimePasswordService.getInformedCode()
+              }
+          })
+          .subscribe({
+            next: () => {
+              this.leadRemoveSuccessSubscription.next({});
+            },
+            error: (err) => {
+                  
+              let message = '';
+              let displayOneTimePasswordDialog = true;
+
+              switch (err.statusCode) {
+                case environment.oneTimePassword.otpInvalidStatusCode: { message = ErrorMessages.CodigoInvalido; break; }
+                case environment.oneTimePassword.otpExpiredStatusCode: { message = ErrorMessages.CodigoInvalido; break; }
+                case environment.oneTimePassword.otpChallengeStatusCode: { message = ''; break; }
+                default: { displayOneTimePasswordDialog = false;
+                  //TODO: Implement a Global error handler
+                  //TODO: Next, throw an error from here so the global error handler can catch and handle it more properly
+                  //this.notificationPanelService.show(`${ErrorMessages.ErroAoProcessarSolicitacao}. ${ErrorMessages.EntreEmContatoSuporteAdm}`, null!, null!);
                 }
-               });
+              }
 
+              if (displayOneTimePasswordDialog) {
+                    
+                setTimeout(() => {
+                  this.oneTimePasswordService
+                      .openDialog({
+                        onSendCodeRequest: () => { this.removeLead(lead, true); },
+                          resource: Permissions.Delete
+                      })
+                      .subscribe(_ => this.oneTimePasswordService.setMessage(message));
+                }, 100);
+
+              }
+            }
+          });
+    };
+
+    if (!skipConfirmation) {
+      this.promptService.openYesNoDialog(
+        `Deseja realmente remover o lead '${lead.razaoSocial}'?`,
+        () => {
+          action();
+        },
+        () => {},
+        "Confirmação de exclusão"
+      );
+    } else {
+      action();
+    }
   }
 }
 
